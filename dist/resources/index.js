562fff8056cf4b380ec823dcf02c7f5cf0ed360e
Tung Pham Thanh <Tung Pham Thanh>
2016-09-19 19:53:41 +0700

    update admin

--- dist/resources/index.js
+++ dist/resources/index.js
@@ -1,7 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     function configure(aurelia) {
-        aurelia.globalResources('./ValueConverter/take', './ValueConverter/currency-format', './ValueConverter/convert-to-image', './ValueConverter/to-length', './ValueConverter/json', './ValueConverter/take-from-to', './ValueConverter/toGender', './ValueConverter/date-format', './CustomAttributes/select2', './CustomAttributes/bootstrap-tooltip', './CustomAttributes/datetime-picker', './CustomAttributes/summernote', './ValueConverter/ObjectKeyConverter', './ValueConverter/number', './CustomAttributes/manific-popup');
+        aurelia.globalResources('./ValueConverter/take', './ValueConverter/currency-format', './ValueConverter/convert-to-image', './ValueConverter/to-length', './ValueConverter/json', './ValueConverter/take-from-to', './ValueConverter/date-format', './CustomAttributes/select2', './CustomAttributes/bootstrap-tooltip', './CustomAttributes/datetime-picker', './CustomAttributes/summernote', './ValueConverter/ObjectKeyConverter', './ValueConverter/number', './CustomAttributes/manific-popup');
     }
     exports.configure = configure;
 });
