741e130fa240696a5bfc2d4cb53bbfa657ec111a
tung.pt <phamthanhtung.vienthonga.com>
2016-09-19 16:13:53 +0700

    init web admin

---
+++ WebAdmin/src/resources/index.ts
@@ -0,0 +1,19 @@
+export function configure(aurelia) {
+    aurelia.globalResources(
+    	'./ValueConverter/take',
+        './ValueConverter/currency-format',
+        './ValueConverter/convert-to-image',
+        './ValueConverter/to-length',
+        './ValueConverter/json',
+        './ValueConverter/take-from-to',
+        './ValueConverter/toGender',
+        './ValueConverter/date-format',
+        './CustomAttributes/select2',
+        './CustomAttributes/bootstrap-tooltip',
+        './CustomAttributes/datetime-picker',
+        './CustomAttributes/summernote',
+        './ValueConverter/ObjectKeyConverter',
+        './ValueConverter/number',
+         './CustomAttributes/manific-popup',
+        );
+}
