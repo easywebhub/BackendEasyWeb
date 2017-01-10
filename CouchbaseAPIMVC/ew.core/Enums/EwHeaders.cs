e85fc626709b235561ebccf746747341d642585f
thanh.td <truongducthanh@vienthonga.vn>
2017-01-10 14:55:52 +0700

    update header for api

--- CouchbaseAPIMVC/ew.core/Enums/EwHeaders.cs
+++ CouchbaseAPIMVC/ew.core/Enums/EwHeaders.cs
@@ -8,18 +8,18 @@ namespace ew.core.Enums
 {
     public class EwHeaders
     {
-        public static string X_Paging_TotalItems = "X-Pagination-TotalItems";
-        public static string X_Paging_Count = "X-Pagination-Count";
-        public static string X_Paging_Page = "X-Pagination-Page";
+        public static string X_Paging_Total_Count = "X-Pagination-Total-Count";
+        public static string X_Paging_Total_Pages = "X-Pagination-Total-Pages";
+        public static string X_Paging_Current_Page = "X-Pagination-Current-Page";
         public static string X_Paging_Limit = "X-Pagination-Limit";
         public static string X_Paging_Next = "X-Pagination-Next";
         public static string X_Paging_Prev = "X-Pagination-Prev";
         public static string X_Paging_First = "X-Pagination-First";
         public static string X_Paging_Last = "X-Pagination-Last";
-        public static string X_Status = "X-EW-Status";
-        public static string X_Errors = "X-EW-Errors";
-        public static string X_Message = "X-EW-Message";
-        public static string X_Error_Message = "X-EW-Error-Messages";
+        public static string X_Status = "X-Status";
+        public static string X_Errors = "X-Errors";
+        public static string X_Message = "X-Message";
+        public static string X_Error_Message = "X-Error-Messages";
 
     }
 }
