263ee0760e37c10a154c47cb4f8d8a8ede24dc87
thanhtdvn <truongducthanh88@gmail.com>
2017-02-22 15:52:06 +0700

    add CloudflareInfo config

---
+++ CouchbaseAPIMVC/ew.config/CloudflareInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ew.config
+{
+    public class CloudflareInfo
+    {
+        public static string APIBaseUrl = "https://api.cloudflare.com/client/v4/";
+        public static string EwhDomain = "easyadmincp.com";
+        public static string EwhCloudflareZoneId = "bb628c8b16d899c3fe2cb67ae2993e3d";
+    }
+}
